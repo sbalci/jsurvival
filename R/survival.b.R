79e58bb00791f43924bd487ab0b54d16c6beb22a
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-17 18:40:43 +0300

    Prepared for release at: 2020-07-17 18:40:43

--- R/survival.b.R
+++ R/survival.b.R
@@ -85,8 +85,10 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
                 if (inherits(outcome1, contin)) {
 
-                    if (any(outcome1 != 0 & outcome1 != 1))
+                    if (any(outcome1 != 0 & outcome1 != 1)) {
                         stop('When using continuous variable as an outcome, it must only contain 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
+
+                    }
 
                     uoutcome <- outcome1
 
