439d2d07ee78ca8bedabd964805697794bda9869
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-17 09:47:24 +0300

    Prepared for release at: 2020-07-17 09:47:24

--- R/multisurvival.b.R
+++ R/multisurvival.b.R
@@ -203,61 +203,68 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
         },
 
-        .plot = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
-
-            # plotData <- image$state
-
-            if (is.null(self$options$explanatory) || (length(self$options$outcome) + length(self$options$overalltime) < 2))
-                return()
-
-            if (nrow(self$data) == 0)
-                stop('Data contains no (complete) rows')
-
-            # Check if outcome variable is suitable or stop ----
-            myoutcome2 <- self$options$outcome
-            myoutcome2 <- self$data[[myoutcome2]]
-            myoutcome2 <- na.omit(myoutcome2)
-            # if (class(myoutcome2) == "factor")
-            #     stop("Please use a continuous variable for outcome.")
-            # if (any(myoutcome2 != 0 & myoutcome2 != 1))
-            #     stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
-
-            mydata <- self$data
-
-            formula2 <- jmvcore::constructFormula(terms = self$options$explanatory)
-
-            formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
-
-            formulaL <- jmvcore::toNumeric(formulaL)
-
-            formulaR <- jmvcore::constructFormula(terms = self$options$outcome)
-
-            formulaR <- jmvcore::toNumeric(formulaR)
 
 
-            myformula <- paste("survival::Surv(", formulaL, ",", formulaR, ")")
+        #
+        # .plot = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
+        #
+        #     # plotData <- image$state
+        #
+        #     if (is.null(self$options$explanatory) || (length(self$options$outcome) + length(self$options$overalltime) < 2))
+        #         return()
+        #
+        #     if (nrow(self$data) == 0)
+        #         stop('Data contains no (complete) rows')
+        #
+        #     # Check if outcome variable is suitable or stop ----
+        #     myoutcome2 <- self$options$outcome
+        #     myoutcome2 <- self$data[[myoutcome2]]
+        #     myoutcome2 <- na.omit(myoutcome2)
+        #     # if (class(myoutcome2) == "factor")
+        #     #     stop("Please use a continuous variable for outcome.")
+        #     # if (any(myoutcome2 != 0 & myoutcome2 != 1))
+        #     #     stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
+        #
+        #     mydata <- self$data
+        #
+        #     formula2 <- jmvcore::constructFormula(terms = self$options$explanatory)
+        #
+        #     formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
+        #
+        #     formulaL <- jmvcore::toNumeric(formulaL)
+        #
+        #     formulaR <- jmvcore::constructFormula(terms = self$options$outcome)
+        #
+        #     formulaR <- jmvcore::toNumeric(formulaR)
+        #
+        #
+        #     myformula <- paste("survival::Surv(", formulaL, ",", formulaR, ")")
+        #
+        #
+        #     # https://finalfit.org/reference/hr_plot.html
+        #
+        #     plot <-
+        #         finalfit::hr_plot(
+        #             .data = mydata,
+        #             dependent = myformula,
+        #             explanatory = formula2,
+        #             dependent_label = "Survival",
+        #             table_text_size = 4,
+        #             title_text_size = 14,
+        #             plot_opts = list(ggplot2::xlab("HR, 95% CI"),
+        #                              ggplot2::theme(
+        #                                  axis.title =
+        #                                      ggplot2::element_text(size = 12)
+        #                                        )))
+        #
+        #     print(plot)
+        #     TRUE
+        #
+        # },
+        #
 
 
-            # https://finalfit.org/reference/hr_plot.html
 
-            plot <-
-                finalfit::hr_plot(
-                    .data = mydata,
-                    dependent = myformula,
-                    explanatory = formula2,
-                    dependent_label = "Survival",
-                    table_text_size = 4,
-                    title_text_size = 14,
-                    plot_opts = list(ggplot2::xlab("HR, 95% CI"),
-                                     ggplot2::theme(
-                                         axis.title =
-                                             ggplot2::element_text(size = 12)
-                                               )))
-
-            print(plot)
-            TRUE
-
-        },
 
             # .plot2 = function(image, ggtheme, theme, ...) {  # <-- the plot function
             #
