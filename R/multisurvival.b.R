0785e7663170d3e7c289e8118d6375defc53430f
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-17 10:17:39 +0300

    Prepared for release at: 2020-07-17 10:17:39

--- R/multisurvival.b.R
+++ R/multisurvival.b.R
@@ -87,70 +87,41 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
             # prepare data ----
 
-            # mydata <- self$data
+            mydata <- self$data
+
+            # outcome1 <- self$options$outcome
+            # outcome1 <- mydata[[outcome1]]
+
+            outcomeLevel <- self$options$outcomeLevel
+
+            mydata[["uoutcome"]] <-
+                ifelse(
+                    test = mydata[[self$options$outcome]] == outcomeLevel,
+                    yes = 1,
+                    no = 0
+                )
 
 
-
-
-                uoveralltime <- self$options$overalltime
-
-                uoveralltime <- jmvcore::toNumeric(self$data[[uoveralltime]])
-
-                uthefactor <- self$options$explanatory
-
-                uthefactor <- self$data[[uthefactor]]
-
-                outcome1 <- self$options$outcome
-
-
-                outcome1 <- self$data[[outcome1]]
-
-                outcomeLevel <- self$options$outcomeLevel
-
-                uoutcome <-
-                    ifelse(
-                        test = outcome1 == outcomeLevel,
-                        yes = 1,
-                        no = 0
-                    )
-
-                mydata <- data.frame(myoveralltime = uoveralltime,
-                                     thefactor = uthefactor,
-                                     myoutcome = uoutcome)
-
-                mydata <- jmvcore::naOmit(mydata)
-
-                names(mydata) <- c(self$options$overalltime,
-                                   self$options$explanatory,
-                                   self$options$outcome)
-
-
-
-                results1 <- mydata
-
-                self$results$text$setContent(results1)
-
+            results1 <- mydata
+            self$results$text$setContent(results1)
 
 
             # prepare formula ----
 
             # formula2 <- as.vector(self$options$explanatory)
-            #
+
             # formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
-            #
+
             # formulaL <- jmvcore::toNumeric(formulaL)
-            #
+
             # formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
-            #
+
             # formulaR <- jmvcore::constructFormula(terms = self$options$outcome)
-            #
+
             # formulaR <- jmvcore::toNumeric(formulaR)
-            #
-            # myformula <- paste("Surv(",
-            #                    formulaL,
-            #                    ",",
-            #                    formulaR,
-            #                    ")")
+
+
+            # myformula <- paste("Surv(", formulaL, ",", formulaR, ")")
 
             # results1 <- list(
             #     formula2,
@@ -162,10 +133,11 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             # self$results$text$setContent(results1)
 
             # finalfit multivariate table ----
-            #
+
             # finalfit::finalfit(.data = mydata,
             #                    dependent = myformula,
             #                    explanatory = formula2,
+            #
             #                    # metrics = TRUE
             #                    ) -> tMultivariate
             #
@@ -225,71 +197,70 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
         }
 
+#
+#         ,
+#         .plot = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
+#
+#             # plotData <- image$state
+#
+#             if (is.null(self$options$explanatory) || (length(self$options$outcome) + length(self$options$overalltime) < 2))
+#                 return()
+#
+#             if (nrow(self$data) == 0)
+#                 stop('Data contains no (complete) rows')
+#
+#             # Check if outcome variable is suitable or stop
+#             myoutcome2 <- self$options$outcome
+#             myoutcome2 <- self$data[[myoutcome2]]
+#             myoutcome2 <- na.omit(myoutcome2)
+#
+#             if (class(myoutcome2) == "factor")
+#                 stop("Please use a continuous variable for outcome.")
+#
+#
+#             if (any(myoutcome2 != 0 & myoutcome2 != 1))
+#                 stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
+#
+#             mydata <- self$data
+#
+#             formula2 <- jmvcore::constructFormula(terms = self$options$explanatory)
+#
+#             formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
+#
+#             formulaL <- jmvcore::toNumeric(formulaL)
+#
+#             formulaR <- jmvcore::constructFormula(terms = self$options$outcome)
+#
+#             formulaR <- jmvcore::toNumeric(formulaR)
+#
+#
+#             myformula <- paste("survival::Surv(", formulaL, ",", formulaR, ")")
+#
+#
+#             # https://finalfit.org/reference/hr_plot.html
+#
+#             plot <-
+#                 finalfit::hr_plot(
+#                     .data = mydata,
+#                     dependent = myformula,
+#                     explanatory = formula2,
+#                     dependent_label = "Survival",
+#                     table_text_size = 4,
+#                     title_text_size = 14,
+#                     plot_opts = list(ggplot2::xlab("HR, 95% CI"),
+#                                      ggplot2::theme(
+#                                          axis.title =
+#                                              ggplot2::element_text(size = 12)
+#                                                )))
+#
+#             print(plot)
+#             TRUE
+#
+#         }
 
-        # ,
-        # .plot = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
-        #
-        #     # plotData <- image$state
-        #
-        #     if (is.null(self$options$explanatory) || (length(self$options$outcome) + length(self$options$overalltime) < 2))
-        #         return()
-        #
-        #     if (nrow(self$data) == 0)
-        #         stop('Data contains no (complete) rows')
-        #
-        #     # Check if outcome variable is suitable or stop ----
-        #     myoutcome2 <- self$options$outcome
-        #     myoutcome2 <- self$data[[myoutcome2]]
-        #     myoutcome2 <- na.omit(myoutcome2)
-        #     # if (class(myoutcome2) == "factor")
-        #     #     stop("Please use a continuous variable for outcome.")
-        #     # if (any(myoutcome2 != 0 & myoutcome2 != 1))
-        #     #     stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
-        #
-        #     mydata <- self$data
-        #
-        #     formula2 <- jmvcore::constructFormula(terms = self$options$explanatory)
-        #
-        #     formulaL <- jmvcore::constructFormula(terms = self$options$overalltime)
-        #
-        #     formulaL <- jmvcore::toNumeric(formulaL)
-        #
-        #     formulaR <- jmvcore::constructFormula(terms = self$options$outcome)
-        #
-        #     formulaR <- jmvcore::toNumeric(formulaR)
-        #
-        #
-        #     myformula <- paste("survival::Surv(", formulaL, ",", formulaR, ")")
-        #
-        #
-        #     # https://finalfit.org/reference/hr_plot.html
-        #
-        #     plot <-
-        #         finalfit::hr_plot(
-        #             .data = mydata,
-        #             dependent = myformula,
-        #             explanatory = formula2,
-        #             dependent_label = "Survival",
-        #             table_text_size = 4,
-        #             title_text_size = 14,
-        #             plot_opts = list(ggplot2::xlab("HR, 95% CI"),
-        #                              ggplot2::theme(
-        #                                  axis.title =
-        #                                      ggplot2::element_text(size = 12)
-        #                                        )))
-        #
-        #     print(plot)
-        #     TRUE
-        #
-        # }
 
-
-
-
-
-
-        # ,
-            # .plot2 = function(image, ggtheme, theme, ...) {  # <-- the plot function
+            # ,
+            # .plot2 = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
             #
             # # plotData <- image$state
             #
@@ -348,11 +319,7 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             # }
 
 
-
-
-
-
-            # ,
+        #     ,
         # .plot3 = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
         #
         #     # plotData <- image$state
@@ -363,15 +330,16 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
         #     if (nrow(self$data) == 0)
         #         stop('Data contains no (complete) rows')
         #
-        #     # Check if outcome variable is suitable or stop ----
+        #     # Check if outcome variable is suitable or stop
         #     myoutcome2 <- self$options$outcome
         #     myoutcome2 <- self$data[[myoutcome2]]
         #     myoutcome2 <- na.omit(myoutcome2)
         #
-        #     # if (class(myoutcome2) == "factor")
-        #     #     stop("Please use a continuous variable for outcome.")
-        #     # if (any(myoutcome2 != 0 & myoutcome2 != 1))
-        #     #     stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
+        #     if (class(myoutcome2) == "factor")
+        #         stop("Please use a continuous variable for outcome.")
+        #
+        #     if (any(myoutcome2 != 0 & myoutcome2 != 1))
+        #         stop('Outcome variable must only contains 1s and 0s. If patient is dead or event (recurrence) occured it is 1. If censored (patient is alive or free of disease) at the last visit it is 0.')
         #
         #
         #
@@ -417,9 +385,6 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
         # }
 
 
-
-
-
 # ,
 # .plot4 = function(image, ggtheme, theme, ...) {  # <-- the plot function ----
 #
@@ -431,7 +396,7 @@ multisurvivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 #     if (nrow(self$data) == 0)
 #         stop('Data contains no (complete) rows')
 #
-#     # Check if outcome variable is suitable or stop ----
+#     # Check if outcome variable is suitable or stop
 #     myoutcome2 <- self$options$outcome
 #     myoutcome2 <- self$data[[myoutcome2]]
 #     myoutcome2 <- na.omit(myoutcome2)
