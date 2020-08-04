4b3cdb3b75c7c7ea3dc5cf3f48ada6341db17ce7
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-04 18:01:13 +0300

    WIP at: 2020-08-04 18:01:13

--- R/survival.b.R
+++ R/survival.b.R
@@ -211,6 +211,7 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
                         )
                 )
 
+                return()
 
                 # Continious Explanatory ----
 
